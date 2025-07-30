0527cffa5b03814dd6534b542d5bf9ed2c78d568
Neysel <neyseltheblack@gmail.com>
2025-07-31 00:59:15 +0400

    i changed it so it can work now

--- src/App.js
+++ src/App.js
@@ -8,3 +8,15 @@ export default function App() {
     </div>
   )
 }
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import AuthForm from './components/AuthForm'
+
+// export default function App() {
+//   return (
+// <BrowserRouter basename="/Neysel/react-19-updates_mine_Test">
+//   <Routes>
+//     <Route path="/" element={<AuthForm />} />
+//   </Routes>
+// </BrowserRouter>
+//   )
+// }
